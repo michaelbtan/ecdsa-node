ac402ccabc55e44337326b1bc687b0d1ff7d9e40
Michael Tan <michaeltan@MacBook-Pro-3.fios-router.home>
2023-03-26 23:08:13 -0400

    first commit

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,9 +1,13 @@
 import server from "./server";
+import * as secp from 'ethereum-cryptography/secp256k1';
+import { toHex } from 'ethereum-cryptography/utils';
 
-function Wallet({ address, setAddress, balance, setBalance }) {
+function Wallet({ address, setAddress, balance, setBalance, privateKey, setPrivateKey }) {
   async function onChange(evt) {
-    const address = evt.target.value;
-    setAddress(address);
+    const privateKey = evt.target.value;
+    setPrivateKey(privateKey)
+    const address = toHex(secp.getPublicKey(privateKey))
+    setAddress(address)
     if (address) {
       const {
         data: { balance },
@@ -19,9 +23,17 @@ function Wallet({ address, setAddress, balance, setBalance }) {
       <h1>Your Wallet</h1>
 
       <label>
-        Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
+        Private Key
+        <select onChange={onChange} className="balance">
+          <option value="40b004ffdc550827d5daf7fc3adff1c58ac0d95d247a2efc2bf748d815fc26c9">40b004ffdc550827d5daf7fc3adff1c58ac0d95d247a2efc2bf748d815fc26c9</option>
+          <option value="56ec7292f4ce0b9dc5e6871416d83c8fc89cd8e4b7ebe5fe7f9b67ff96cca8d4">56ec7292f4ce0b9dc5e6871416d83c8fc89cd8e4b7ebe5fe7f9b67ff96cca8d4</option>
+          <option value="463b2f8d529951a8dcc9f6a28be76ca78b2e71c4c9615405a0e275b3cf257d28">463b2f8d529951a8dcc9f6a28be76ca78b2e71c4c9615405a0e275b3cf257d28</option>
+        </select>
       </label>
+
+      <div className="balance">
+        Address: {address.slice(0, 10)}...
+      </div>
 
       <div className="balance">Balance: {balance}</div>
     </div>
--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "04448e1a4e3415ca8eb210b21008c628fc621ca6c0da954d45dc922474f9da698cd5a21a7b26841ea0d3e197a8635ad3775676c63addd76d2f416a9b2fc6a1f87c": 100,
+  "0449c9f3698b37896fba10291daab21210e0084226961c435400897651210e340c5220c10056a1a5deba4e479e6d8ee3d710046a0ca17c1e0176a66b085bcbe68f": 50,
+  "040b414e5c8df92705d91c45da44bf517e0b7c40ddaf656a8f2df5b9ea2d925fd75c64fde3d76164fae39af1c2345f7ee6b53b6f4d3f748e4dcae5f9a1d57ef2dd": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
